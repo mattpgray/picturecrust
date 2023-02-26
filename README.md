55731340ad98798ec826283639c9f17c8be796ce
= <=>
2023-02-26 09:16:10 +0000

    Improve the performance to work on 15x15 puzzles

--- README.md
+++ README.md
@@ -1,19 +1,42 @@
 # PicturecRUST
 
-PicturecRUST is a terrible picture cross solver, written in rust.
+PicturecRUST is a picture cross solver, written in rust.
 
-It works by recursively generating all the rows and columns based on the column and row settings,
-and then comparing them so see if they match.
+It uses the following procedure to solve a picture cross puzzle:
 
-If a board generated from row setting matches a board generated from col settings, we have found
-the winning board.
+1: Generate a list of proposals for each row and column based on the row and column settings.
+   For example, for a 8x8 board with a row setting of (3, 3) has 3 possible configurations:
 
-The current implementation is very slow, and should only be used on 8x8 boards.
+```
+1. ███X███X
+2. ███XX███
+3. X███X███
+```
+2. For the row proposals, generate a board where the state of each cell is known by filling
+   in the cells that overlap. Wfter generating this board, filter out any col proposals that do
+   not match the board. For example, for the proposals above, the generated row would be:
+
+```
+?██??██?
+?██??██?
+?██??██?
+```
+
+3. Do the same thing but with the row proposals swapped, using the filtered col proposals from step 2
+
+4. Check how many possible boards are left by multiplying all the number of proposals. This will
+   be two different values for each of the row and col proposals. If there are 0 valid boards,
+   the settings are invalid and we can stop. If there are exactly 1 valid row and col board, we have
+   found our board and we are done. If there number of valid boards has not changed since the last
+   stage, we are stuck and we start brute forcing in step 5.
+
+5. Generate all possible board given the row and col proposals. Check if there is a board that
+   exists in both the sets of board. If there is, we have found the winning board. If there is not
+   the settings are incorrect.
 
 ## TODO
 
 - Add command line options to avoid having to specifiy the board in code directly.
   (The coolest thing would be to take a picture of a board and load the settings from that.
    I doubt that will ever happen in this project though. Maybe just loading from json.)
-- Speed up the solving time. Sould work on 15x15 boards within a reasonable time frame.
 
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ enum CellState {
 
 type Setting = Vec<i32>;
 
+fn all_proposals(len: usize, all_settings: &Vec<Setting>) -> Vec<Vec<Proposal>> {
+    Vec::from_iter(all_settings.iter().map(|settings| proposals(len, settings)))
+}
+
 fn proposals(len: usize, settings: &Setting) -> Vec<Proposal> {
     gen_proposal(len, settings)
 }
@@ -68,30 +72,75 @@ impl BoardSolver {
     }
 
     fn solve(&self) -> Option<Board> {
-        let row_boards = self.generate_row_boards();
-        let col_boards = self.generate_col_boards();
+        let rows = self.row_settings.len();
+        let cols = self.col_settings.len();
+        // All possible proposals
+        let mut row_proposals = all_proposals(cols, &self.row_settings);
+        let mut col_proposals = all_proposals(rows, &self.col_settings);
+
+        // Filter down the list of possible proposals until we reach the limit.
+        let mut n_row_boards = n_boards(&row_proposals);
+        let mut n_col_boards = n_boards(&row_proposals);
+        loop {
+            (row_proposals, col_proposals) = reduce_proposals(row_proposals, col_proposals);
+            let next_row_boards = n_boards(&row_proposals);
+            let next_col_boards = n_boards(&col_proposals);
+
+            // Invalid board settings.
+            if next_row_boards == 0 || next_col_boards == 0 {
+                return None;
+            }
+
+            // The base case. We are done.
+            if next_row_boards == 1 && next_col_boards == 1 {
+                break;
+            }
+            // If we cannot reduce further we have to leave the loop
+            if next_row_boards == n_row_boards && next_col_boards == n_col_boards {
+                break;
+            }
+
+            n_row_boards = next_row_boards;
+            n_col_boards = next_col_boards;
+        }
+
+        // We can now use brute force to find the winning solution.
+        // 1. Generate all possible boards given the row proposals.
+        // 2. Generate all possible boards given the col proposals.
+        // 3. Find the board that is in both of these sets of boards.
+        //    That is the winning board.
+        let row_boards = self.generate_row_boards(rows, cols, &row_proposals);
+        let col_boards = self.generate_col_boards(rows, cols, &col_proposals);
         let row_sets: HashSet<Board> = HashSet::from_iter(row_boards);
         for col_board in &col_boards {
             if row_sets.contains(col_board) {
-                return Some(col_board.clone())
+                return Some(col_board.clone());
             }
         }
         None
     }
 
-    fn generate_row_boards(&self) -> Vec<Board> {
-        let rows = self.row_settings.len();
-        let cols = self.col_settings.len();
-
-        let row_proposals = Vec::from_iter(
-            self.row_settings
-                .iter()
-                .map(|settings| proposals(cols, settings)),
-        );
-
+    fn generate_row_boards(
+        &self,
+        rows: usize,
+        cols: usize,
+        row_proposals: &Vec<Vec<Proposal>>,
+    ) -> Vec<Board> {
         let mut boards: Vec<Board> = Vec::new();
         let empty_board = Board::new(rows, cols);
-        self.prop_boards_rows(&mut boards, empty_board, &row_proposals, 0);
+        self.prop_boards_rows(&mut boards, empty_board, row_proposals, 0);
+        boards
+    }
+
+    fn generate_col_boards(
+        &self,
+        rows: usize,
+        cols: usize,
+        col_proposals: &Vec<Vec<Proposal>>,
+    ) -> Vec<Board> {
+        let mut boards: Vec<Board> = Vec::new();
+        let empty_board = Board::new(rows, cols);
+        self.prop_boards_cols(&mut boards, empty_board, col_proposals, 0);
         boards
     }
 
@@ -138,22 +187,6 @@ impl BoardSolver {
             self.prop_boards_cols(boards, next_board, &props[1..], col + 1);
         }
     }
-
-    fn generate_col_boards(&self) -> Vec<Board> {
-        let rows = self.row_settings.len();
-        let cols = self.col_settings.len();
-
-
-        let col_proposals = Vec::from_iter(self
-            .col_settings
-            .iter()
-            .map(|settings| proposals(rows, settings)));
-
-        let mut boards: Vec<Board> = Vec::new();
-        let empty_board = Board::new(rows, cols);
-        self.prop_boards_cols(&mut boards, empty_board, &col_proposals, 0);
-        boards
-    }
 }
 
 #[derive(Clone, PartialEq, Eq, Hash)]
@@ -192,12 +225,104 @@ impl Board {
     }
 }
 
+fn n_boards(all_proposals: &Vec<Vec<Proposal>>) -> usize {
+    let mut tot: usize = 1;
+    for props in all_proposals {
+        tot *= props.len();
+    }
+    tot
+}
+
+fn reduce_board(
+    board_proposals: &Vec<Vec<Proposal>>,
+    check_proposals: &Vec<Vec<Proposal>>,
+    transpose: bool,
+) -> Vec<Vec<Proposal>> {
+    let mut board = if transpose {
+        Board::new(check_proposals.len(), board_proposals.len())
+    } else {
+        Board::new(board_proposals.len(), check_proposals.len())
+    };
+
+    // Generate a board that only contains cell states that we know for certain
+    // as all of the proposals for this row/col match.
+    for (i, props) in board_proposals.iter().enumerate() {
+        for (prop_idx, prop) in props.iter().enumerate() {
+            if prop_idx == 0 {
+                for (j, cell) in prop.iter().enumerate() {
+                    if transpose {
+                        board.set_cell(j, i, cell.clone());
+                    } else {
+                        board.set_cell(i, j, cell.clone());
+                    }
+                }
+                continue;
+            }
+            for (j, cell) in prop.iter().enumerate() {
+                let board_cell = if transpose {
+                    board.get_cell(j, i)
+                } else {
+                    board.get_cell(i, j)
+                };
+
+                if board_cell.to_owned() == CellState::Unknown {
+                    continue;
+                }
+
+                if cell.to_owned() != board_cell.to_owned() {
+                    if transpose {
+                        board.set_cell(j, i, CellState::Unknown);
+                    } else {
+                        board.set_cell(i, j, CellState::Unknown);
+                    }
+                }
+            }
+        }
+    }
+
+    // Now we validate against the set of settings to be able to reduce how many options they have.
+    let mut valid_props: Vec<Vec<Proposal>> = Vec::new();
+    for (j, prop) in check_proposals.iter().enumerate() {
+        let valid_prop = prop.iter().cloned().filter(|prop| {
+            for (i, cell) in prop.iter().enumerate() {
+                let board_cell = if transpose {
+                    board.get_cell(j, i)
+                } else {
+                    board.get_cell(i, j)
+                };
+
+                if board_cell.to_owned() == CellState::Unknown {
+                    continue;
+                }
+
+                if cell.to_owned() != board_cell.to_owned() {
+                    return false;
+                }
+            }
+            true
+        });
+        valid_props.push(Vec::from_iter(valid_prop));
+    }
+
+    valid_props
+}
+
+fn reduce_proposals(
+    row_proposals: Vec<Vec<Proposal>>,
+    col_proposals: Vec<Vec<Proposal>>,
+) -> (Vec<Vec<Proposal>>, Vec<Vec<Proposal>>) {
+    let col_proposals = reduce_board(&row_proposals, &col_proposals, false);
+    let row_proposals = reduce_board(&col_proposals, &row_proposals, true);
+    (row_proposals, col_proposals)
+}
+
 fn main() {
     let row_settings = vec![
         vec![9],
         vec![2, 1, 1, 1, 2],
         vec![1, 1, 1, 1, 1],
         vec![2, 1, 1, 1, 2],
+        vec![2, 2, 1, 2, 2],
         vec![13],
         vec![1, 1],
         vec![15],
@@ -208,7 +333,6 @@ fn main() {
         vec![6, 6],
         vec![15],
         vec![13],
-        vec![],
     ];
     let col_settings = vec![
         vec![8],
